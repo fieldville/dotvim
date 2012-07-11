4516ad5536b4a7d9545ceee89bf584035dab6677
naoki_ichihara <naoki_ichihara@sobal.co.jp>
2012-07-11 10:33:55 +0900

    Modified skel for pm

--- skel/skel.pm
+++ skel/skel.pm
@@ -1,8 +1,13 @@
-package Hoge:: ;
-
+package Hoge::Base::Fuga;
 use strict;
 use warnings;
+use base qw( Hoge::Base );
+
 use Carp;
 
+sub new {
+    my $class = shift;
+    bless {}, $class;
+}
 
 1;
